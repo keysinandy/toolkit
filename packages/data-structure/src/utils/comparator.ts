17ca9d729091f3410681a8714f8f065af9ae73b7
Key Sin <keysinandy@gmail.com>
2023-09-15 17:43:02 +0800

    feat: add comparator test case

--- packages/data-structure/src/utils/comparator.ts
+++ packages/data-structure/src/utils/comparator.ts
@@ -2,10 +2,15 @@ export interface CompareFn<T = unknown> {
   (a: T, b: T): number;
 }
 
+const defaultCompare: CompareFn = (a, b) => {
+  if (a === b) return 0;
+  return (a as number) > (b as number) ? 1 : -1;
+};
+
 export class Comparator<T = unknown> {
   compare: CompareFn<T>;
   constructor(compare?: CompareFn<T>) {
-    this.compare = compare ?? ((a, b) => (a as number) - (b as number));
+    this.compare = compare ?? defaultCompare;
   }
 
   equal(a: T, b: T) {
@@ -20,11 +25,16 @@ export class Comparator<T = unknown> {
     return this.compare(a, b) < 0;
   }
 
-  greaterOrEqualThan(a: T, b: T) {
+  greaterThanOrEqual(a: T, b: T) {
     return this.compare(a, b) >= 0;
   }
 
-  lessOrEqualThan(a: T, b: T) {
+  lessThanOrEqual(a: T, b: T) {
     return this.compare(a, b) <= 0;
+  }
+
+  reverse() {
+    const fn = this.compare;
+    this.compare = (a, b) => fn(b, a);
   }
 }
